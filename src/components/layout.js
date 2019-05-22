275c25f61eafb99e5dad887854b1ff183fa98f9b
Julian Furchert <work@julianfurchert.de>
2019-05-22 16:38:55 +0200

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from "./Header"
 
 const Layout = ({ children }) => (
   <StaticQuery
---
+++ src/snipptes.js
@@ -0,0 +1,27 @@
+import exampleVideo from './videos/example.mp4';
+import exampleImage from './videos/example.jpg';
+
+
+export default [
+  {
+    title: 'Example',
+    video: exampleVideo,
+    videoPoster: exampleImage,
+    fullscreen: 'https://f569k.codesandbox.io/',
+    source: 'https://codesandbox.io/s/example-f569k'
+  },
+  {
+    title: 'Example 1',
+    video: exampleVideo,
+    videoPoster: exampleImage,
+    fullscreen: 'https://f569k.codesandbox.io/',
+    source: 'https://codesandbox.io/s/example-f569k'
+  },
+  {
+    title: 'Example 2',
+    video: exampleVideo,
+    videoPoster: exampleImage,
+    fullscreen: 'https://f569k.codesandbox.io/',
+    source: 'https://codesandbox.io/s/example-f569k'
+  },
+]
