07a7d544c0fb6c347c420a2c8a0c4acff84f33e4
Julian Furchert <work@julianfurchert.de>
2019-05-23 00:13:05 +0200

    update useScrollPosition

--- src/hooks/useScrollPosition.js
+++ src/hooks/useScrollPosition.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react"
 import debounce from 'lodash.debounce';
 
 const useScrollPosition = () =>{
-  const [position, setScrollPosition] = useState({x: window.pageXOffset, y: window.pageYOffset});
+  const [position, setScrollPosition] = useState({x: 0, y: 0});
 
   useEffect(() => {
     function handleScroll() {
-      setScrollPosition({x: window.pageXOffset, y: window.pageYOffset})
+      if(typeof window !== 'undefined' && window){
+        setScrollPosition({x: window.pageXOffset, y: window.pageYOffset})
+      };
     }
     window.addEventListener('scroll', debounce(handleScroll, 10));
     return () => window.removeEventListener('scroll', debounce(handleScroll, 10));
